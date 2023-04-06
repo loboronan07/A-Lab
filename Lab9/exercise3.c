01c0b2c9c6896777d5fb738a3860c50bf9af268e
Ronan Lobo <113038897+loboronan07@users.noreply.github.com>
2023-04-06 09:21:18 +0530

    Upload of L9
    Uploaded L9/S and L9/E3

---
+++ Lab9/exercise3.c
@@ -0,0 +1,102 @@
+/* Lab Exercise 3
+	Write a program to construct the closed hash table. Find the number of key
+	comparisons in successful search and unsuccessful search.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define EMPTY -5555
+
+int opcount;
+
+int* createhash(int*, int, int);
+int search(int*, int, int);
+
+int main() {
+	int *arr, n;
+	int *hash, key;
+	int ele;
+
+	printf("Enter the number of elements in the array: ");
+	scanf("%d", &n);
+
+	arr = (int*) calloc(n, sizeof(int));
+
+	printf("Enter %d elements into the array:\n", n);
+	for(int i=0; i<n; i++) {
+		scanf("%d", arr+i);
+	}
+
+	printf("Enter the value of key for the hash table: ");
+	scanf("%d", &key);
+
+	hash = createhash(arr, n, key);
+
+	printf("Enter the search element: ");
+	scanf("%d", &ele);
+
+	opcount = 0;
+
+	if(search(hash, key, ele)) {
+		printf("Search successfull with operation count %d...\n", opcount);
+	}
+	else {
+		printf("Search unsuccessfull with operation count %d...\n", opcount);
+	}
+
+	return 0;
+}
+
+int* createhash(int* arr, int n, int key) {
+	if(key < n) {
+		printf("Number of elements is more than the key value.\n");
+		printf("Closed Hashing Not Possible.");
+		printf("Exiting from Program...\n");
+		exit(1);
+	}
+
+	int* hash = (int*) calloc(key, sizeof(int));
+
+	for(int i=0; i<key; i++) {
+		hash[i] = EMPTY;
+	}
+
+
+	for(int i=0; i<n; i++) {
+		int pos = arr[i] % key;
+
+		int j=pos;
+
+		do {
+			if(hash[j] == EMPTY) {
+				hash[j] = arr[i];
+				break;
+			}
+
+			j = (j+1) % key;
+		} while(j != pos);
+	}
+
+	return hash;
+}
+
+int search(int* hash, int key, int ele) {
+	int pos = ele % key;
+
+	int i = pos;
+
+	do {
+		opcount++;
+		if(hash[i] == ele) {
+			return 1;
+		}
+		else if(hash[i] == EMPTY) {
+			return 0;
+		}
+
+		i = (i+1) % key;
+	} while(i != pos);
+
+	return 0;
+}
---
+++ Lab9/solved.c
@@ -0,0 +1,66 @@
+/* Solved Exercise
+	Write a program to sort set of integers using comparison counting algorithm.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int* countingsort(int*, int);
+void getminmax(int*, int, int*, int*);
+
+int main() {
+	int *arr, n;
+
+	printf("Enter the number of elements in array: ");
+	scanf("%d", &n);
+
+	arr = (int*) calloc(n, sizeof(int));
+
+	printf("Enter %d elements into array: ", n);
+	for(int i=0; i<n; i++) {
+		scanf("%d", arr+i);
+	}
+
+	printf("\nArray before sorting:\n");
+	for(int i=0; i<n; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+
+	arr = countingsort(arr, n);
+
+	printf("\nArray after sorting:\n");
+	for(int i=0; i<n; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+
+	free(arr);
+
+	return 0;
+}
+
+int* countingsort(int* arr, int n) {
+	int* counts = (int*) calloc(n, sizeof(int));
+
+	for(int i=0; i<n-1; i++) {
+		for(int j=i+1; j<n; j++) {
+			if(arr[i] < arr[j]) {
+				counts[j] += 1;
+			}
+			else {
+				counts[i] += 1;
+			}
+		}
+	}
+
+	int* out = (int*) calloc(n, sizeof(int));
+
+	for(int i=0; i<n; i++) {
+		out[counts[i]] = arr[i];
+	}
+
+	free(arr);
+
+	return out;
+}
