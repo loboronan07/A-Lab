82b507955de8da39ccb1bc406856a88233739b50
Ronan Lobo <113038897+loboronan07@users.noreply.github.com>
2023-05-10 23:18:02 +0530

    Update additional1.c

--- Lab10/additional1.c
+++ Lab10/additional1.c
@@ -17,11 +17,11 @@ int main() {
 	printf("Enter the number of elements: ");
 	scanf("%d", &n);
 
-	weight = (int*) calloc(n, sizeof(int));
-	value = (int*) calloc(n, sizeof(int));
+	weight = (int*) calloc(n+1, sizeof(int));
+	value = (int*) calloc(n+1, sizeof(int));
 
 	printf("Enter the elements as weight, value:\n");
-	for(int i=0; i<n; i++) {
+	for(int i=1; i<=n; i++) {
 		scanf("%d %d", weight+i, value+i);
 	}
 
@@ -30,31 +30,22 @@ int main() {
 
 	printf("Knapsack can contain elements with max value of %d.\n", knapsack());
 
-    free(weight);
-    free(value);
-
 	return 0;
 }
 
 int knapsack() {
 	int** f = initmat(n+1, B+1);
-	
-    for(int i=1; i<n+1; i++) {
-        for(int j=1; j<B+1; j++) {
-            f[i][j] = -1;
-        }
-    }
 
 	return mfknapsack(f, n, B);
 }
 
 int mfknapsack(int** f, int i, int j) {
     if(f[i][j] < 0) {
-        if(j < weight[i-1]) {
+        if(j < weight[i]) {
             f[i][j] = mfknapsack(f, i-1, j);
         }
         else {
-            f[i][j] = MAX(mfknapsack(f, i-1, j), value[i-1] + mfknapsack(f, i-1, j-weight[i-1]));
+            f[i][j] = MAX(mfknapsack(f, i-1, j), value[i] + mfknapsack(f, i-1, j-weight[i]));
         }
     }
     
@@ -66,6 +57,12 @@ int** initmat(int m, int n) {
 
 	for(int i=0; i<m; i++) {
 		mat[i] = (int*) calloc(n, sizeof(int));
+		if(i == 0) {
+			continue;
+		}
+		for(int j=1; j<n; j++) {
+        		mat[i][j] = -1;
+        	}
 	}
 
 	return mat;
